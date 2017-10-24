d130f0292553d29a761f6feb529a97218de043f6
Trashvin <m3lles@gmail.com>
2017-10-24 19:28:10 +0800

    TEST

--- app/index.js
+++ app/index.js
@@ -1,2 +1,9 @@
 import "./styles/styles.scss";
 console.log("entry point");
+
+const showAlert = () => {
+    console.log(window.test);
+    console.log("Hello, world!");
+}
+
+window.showAlert = showAlert;
